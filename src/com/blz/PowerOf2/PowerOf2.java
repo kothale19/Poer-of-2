ba400332a88e80c960a4d894f6eaeb77ed5e2941
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-19 10:15:46 +0530

    Added: power table of 2 logic

--- src/com/blz/PowerOf2/PowerOf2.java
+++ src/com/blz/PowerOf2/PowerOf2.java
@@ -1,13 +1,26 @@
 package com.blz.PowerOf2;
 
+import java.util.Scanner;
+
 public class PowerOf2 {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to PowerOf2 Program!!");
-		
-		
-		
+		int N;
+		int p;
+		int power = 1;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		 N = scan.nextInt();
+	    System.out.println("Enter the Power:");
+	     p = scan.nextInt();
+		for(int i=1; i<=p; i++) {
 
-	}
+			power =N*power;
+		}
+			System.out.println("Power Table of 2 is:"+power);
+		
+	
 
+}
 }
